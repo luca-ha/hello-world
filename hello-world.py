7c7c162106540cb9b19e058c0c016a678730d0c2
luca-ha <luca.hartman@hotmail.com>
2018-11-11 13:21:31 +0100

    corrected the python script

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,10 @@
 #this programm shows "hello world"
 
+#print("hello world")
 def hello_world():
-    print("hello world\n")
+    print("hello world")
+    if 1 == 2:
+        print("Wait, what ?")
 
-if __name == '__name__':
-    hello-world()
+if __name__ == '__main__' :
+    hello_world()
